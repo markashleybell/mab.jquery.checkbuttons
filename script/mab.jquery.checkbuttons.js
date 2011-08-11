472e6c6192bb39b1f9b9f58eb2a711eee67e2ec5
Mark Bell <me@markashleybell.com>
2011-08-11 12:42:52 +0100

    Styled and updated

---
+++ script/mab.jquery.checkbuttons.js
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+// CheckButtons 1.0
+// Version 1.0
+// @requires jQuery v1.6.2
+// 
+// Copyright (c) 2011 Mark Ashley Bell
+// Examples and docs at: https://github.com/markashleybell/mab.jquery.checkbuttons
+// 
+// Dual licensed under the MIT and GPL licenses:
+// http://www.opensource.org/licenses/mit-license.php
+// http://www.gnu.org/licenses/gpl.html
+///////////////////////////////////////////////////////////////////////////////////////////
+
+(function ($) {
+
+    $.fn.checkButtons = function (params) {
+
+        this.find('label').each(function () {
+
+            var label = $(this);
+            var checkbox = $('#' + label.attr('for'));
+            var offset = label.offset();
+
+            label.prepend('<span></span>');
+            label.addClass((checkbox.is(':checked')) ? 'checked' : 'unchecked');
+
+            // Hide the checkbox off screen
+            checkbox.css({ position: 'absolute', top: offset.top, left: '-1000px', outline: 'none' });
+
+        }).bind('click', function (event) {
+
+            // We don't need to manually check the checkbox, just let the default
+            // behaviour when a label is clicked handle it (for now)
+            var label = $(this);
+
+            if (label.hasClass('checked')) {
+                label.removeClass('checked').addClass('unchecked');
+            } else {
+                label.removeClass('unchecked').addClass('checked');
+            }
+
+        });
+        
+        return this;
+
+    };
+
+})(jQuery);
